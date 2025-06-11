779c30677ec220efe440351bd9bf85bfd28a5b91
joseh33 <josehsejoh33@gmail.com>
2025-06-12 02:46:49 +0300

    Add login, register and protected route with JWT

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+
+  if (!token) {
+    return res.status(401).json({ msg: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // attach user info to request
+    next();
+  } catch (err) {
+    res.status(403).json({ msg: "Invalid or expired token" });
+  }
+}
+
+module.exports = authenticateToken;
